c77252784ca94d09499ecb2607c80d02f6bb7585
ffcabbar <ffcabbar@gmail.com>
2022-04-30 03:40:31 +0300

    updates app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import styles from './App.module.scss';
+import { Card, CoinList, Header } from './components';
 
 const App = () => {
-  return <div className={styles.App}>init</div>;
+  return (
+    <div>
+      <main className={styles.main}>
+        {/* <div className={styles.figure}></div> */}
+        {/* <div className={styles.bg}></div> */}
+        <div className={styles.container}>
+          <Header />
+          <div className={styles.innerSection}>
+            <div className={styles.descriptionSection}>
+              <h1>Now you can track all your cryptos here!</h1>
+              <h2>Just enter the cryptocurrency code on the form to the right.</h2>
+            </div>
+            <div>
+              <Card />
+            </div>
+          </div>
+          <div className={styles.coinListSection}>
+            <CoinList />
+          </div>
+        </div>
+      </main>
+      {/* <div className={styles.footer}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore necessitatibus in quas,
+        similique, porro, earum sequi ullam quasi numquam molestias illum provident cum odit cumque
+        illo nisi sapiente consectetur iure.
+      </div> */}
+    </div>
+  );
 };
 
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 import { Header } from './Header/Header';
+import { Card } from './Card/Card';
+import { CoinList } from './CoinList/CoinList';
 
-export { Header };
+export { Header, Card, CoinList };
