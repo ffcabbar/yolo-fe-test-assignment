14a92798420f1c43f8006498de1ec7acfe783006
ffcabbar <ffcabbar@gmail.com>
2022-05-01 01:19:24 +0300

    adds App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import styles from './App.module.scss';
-import { Card, CoinList, Header } from './components';
+import { Card, CoinList, Footer, Header } from './components';
 
 const App = () => {
   return (
     <div>
       <main className={styles.main}>
-        {/* <div className={styles.figure}></div> */}
-        {/* <div className={styles.bg}></div> */}
         <div className={styles.container}>
           <Header />
           <div className={styles.innerSection}>
@@ -23,11 +21,7 @@ const App = () => {
           </div>
         </div>
       </main>
-      {/* <div className={styles.footer}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore necessitatibus in quas,
-        similique, porro, earum sequi ullam quasi numquam molestias illum provident cum odit cumque
-        illo nisi sapiente consectetur iure.
-      </div> */}
+      <Footer />
     </div>
   );
 };
