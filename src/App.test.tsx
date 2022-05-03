d40210bf7808beda099534058a4a87a93acae4f4
ffcabbar <ffcabbar@gmail.com>
2022-05-03 17:49:03 +0300

    fixes test and lottie-react library issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MockedProvider } from '@apollo/react-testing';
+import { GET_COIN_PRICE_QUERY } from './graphql/Queries';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mocks = [
+  {
+    request: {
+      query: GET_COIN_PRICE_QUERY,
+      variables: { code: 'BNB' }
+    },
+    result: {
+      data: {
+        markets: [
+          {
+            id: 'binance_bnb_eur',
+            baseSymbol: 'BNB',
+            ticker: {
+              lastPrice: '414.90000000',
+              __typename: 'Ticker'
+            },
+            __typename: 'Market'
+          }
+        ]
+      }
+    }
+  },
+  {
+    request: {
+      query: GET_COIN_PRICE_QUERY,
+      variables: { code: 'asd' }
+    },
+    error: new Error('Something went wrong')
+  }
+];
+
+it('runs the mocked query', () => {
+  render(
+    <MockedProvider mocks={mocks}>
+      <App />
+    </MockedProvider>
+  );
+
+  const titleElement = screen.getByText(/Now you can track all your cryptos here!/i);
+  expect(titleElement).toBeInTheDocument();
 });
